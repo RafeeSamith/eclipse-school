9404fbaf25df3617aa7119a6079c78b9d93b3337
Rafee <rafeesamith@gmail.com>
2022-08-20 02:19:45 +0300

    update: I HATE PICKLE

--- __main__.py
+++ __main__.py
@@ -6,4 +6,3 @@ from modules import fileHandling
 
 #{"empno": 0, "email": "test", "role": "admin"}
 tkmenus.displayFile("accounts_user", "jdkhjaslfl")
-#tkmenus.updateAccount_user({"empno": 0, "email": "test", "role": "admin"})
